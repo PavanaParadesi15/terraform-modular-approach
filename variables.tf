5d889bae7f1e506aea5bcf1f56d5290ecda66333
Pavana Paradesi <pp318@student.le.ac.uk>
2025-02-04 12:07:50 +0530

    created files

---
+++ variables.tf
@@ -0,0 +1,10 @@
+// Creating variables for the EC2 instance
+
+variable "ami_value" {
+    description = "value for the ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance_type"
+}
+
